413c665fe71ff5eb75d5ff199cd7bfa18bd66efe
Vitaly Domnikov <dotcypress@gmail.com>
2020-10-26 15:28:16 -0700

    cleanup tests

--- qtff/src/file.rs
+++ qtff/src/file.rs
@@ -697,12 +697,5 @@ mod tests {
         let mut f = File::open("src/testdata/empty.mov").unwrap();
         let movie_data = f.get_movie_data().unwrap();
         assert_eq!(movie_data.tracks.len(), 3);
-    }
-
-    #[test]
-    fn test_file_fragmented_mp4() {
-        let mut f = File::open("../../video/00000012.mp4").unwrap();
-        let movie_data = f.get_movie_data().unwrap();
-        assert_eq!(movie_data.tracks.len(), 1);
-    }
+    } 
 }
