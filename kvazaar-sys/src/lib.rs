1e433e8b0872fb00966f6475f6eb7a95c0fd2fd6
Vitaly Domnikov <dotcypress@gmail.com>
2020-11-10 11:27:02 -0800

    cleanup

--- kvazaar-sys/src/lib.rs
+++ kvazaar-sys/src/lib.rs
@@ -1,2 +1,2 @@
-#![allow(non_snake_case, non_upper_case_globals, non_camel_case_types)]
+#![allow(non_snake_case, non_upper_case_globals, non_camel_case_types, cognitive_complexity)]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
