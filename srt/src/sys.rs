7c87c31a4be7d0ad86e693c75fefdca0f3705013
Christopher Brown <ccbrown112@gmail.com>
2021-09-05 18:31:52 -0400

    use srt epoll api

--- srt/src/sys.rs
+++ srt/src/sys.rs
@@ -2,10 +2,28 @@ pub use libc::{c_char as char, c_int as int, c_void as void, sockaddr, sockaddr_
 
 pub type SRTSOCKET = int;
 
+#[cfg(feature = "async")]
+#[repr(C)]
+#[allow(non_camel_case_types)]
+pub enum SRT_EPOLL_OPT {
+    SRT_EPOLL_IN = 1,
+    SRT_EPOLL_OUT = 4,
+    SRT_EPOLL_ERR = 8,
+}
+
+#[cfg(feature = "async")]
+pub type SYSSOCKET = int;
+
 #[repr(C)]
 #[allow(non_camel_case_types)]
 pub enum SRT_SOCKOPT {
+    #[cfg(feature = "async")]
+    SNDSYN = 1,
+    #[cfg(feature = "async")]
+    RCVSYN = 2,
     RCVBUF = 6,
+    #[cfg(feature = "async")]
+    EVENT = 18,
     TSBPDMODE = 22,
     PASSPHRASE = 26,
     TLPKTDROP = 31,
@@ -41,5 +59,28 @@ extern "C" {
         lsn: SRTSOCKET,
         hook_fn: extern "C" fn(*mut void, SRTSOCKET, int, *const sockaddr, *const char) -> int,
         hook_opaque: *mut void,
+    ) -> int;
+}
+
+#[cfg(feature = "async")]
+#[link(name = "srt", kind = "static")]
+extern "C" {
+    pub fn srt_epoll_create() -> int;
+    pub fn srt_epoll_release(eid: int) -> int;
+    pub fn srt_epoll_add_ssock(eid: int, s: SYSSOCKET, events: *const int) -> int;
+    pub fn srt_epoll_add_usock(eid: int, s: SRTSOCKET, events: *const int) -> int;
+    pub fn srt_epoll_update_usock(eid: int, s: SRTSOCKET, events: *const int) -> int;
+    pub fn srt_epoll_remove_usock(eid: int, s: SRTSOCKET) -> int;
+    pub fn srt_epoll_wait(
+        eid: int,
+        readfds: *mut SRTSOCKET,
+        rnum: *mut int,
+        writefds: *mut SRTSOCKET,
+        wnum: *mut int,
+        ms_timeout: i64,
+        lrfds: *mut SYSSOCKET,
+        lrnum: *mut int,
+        lwfds: *mut SYSSOCKET,
+        lwnum: *mut int,
     ) -> int;
 }
