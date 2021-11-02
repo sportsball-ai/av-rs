c716c68100ac23268082defe1aca204176c059cb
Christopher Brown <ccbrown112@gmail.com>
2021-11-01 20:22:01 -0400

    cargo fmt

--- kvazaar-sys/src/lib.rs
+++ kvazaar-sys/src/lib.rs
@@ -1,2 +1,8 @@
-#![allow(non_snake_case, non_upper_case_globals, non_camel_case_types, clippy::unreadable_literal, clippy::cognitive_complexity)]
+#![allow(
+    non_snake_case,
+    non_upper_case_globals,
+    non_camel_case_types,
+    clippy::unreadable_literal,
+    clippy::cognitive_complexity
+)]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
