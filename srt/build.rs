1bcf6c643cf71009a57ee9f0354ca224948978f3
ccbrown <ccbrown112@gmail.com>
2021-12-24 13:53:07 -0500

    add m1 support for srt crate

--- srt/build.rs
+++ srt/build.rs
@@ -1,7 +1,11 @@
 fn main() {
     #[cfg(target_os = "macos")]
     {
-        println!("cargo:rustc-link-search={}/vendor/macos/lib", env!("CARGO_MANIFEST_DIR"));
+        #[cfg(target_arch = "x86_64")]
+        println!("cargo:rustc-link-search={}/vendor/macos-x86_64/lib", env!("CARGO_MANIFEST_DIR"));
+        #[cfg(target_arch = "aarch64")]
+        println!("cargo:rustc-link-search={}/vendor/macos-aarch64/lib", env!("CARGO_MANIFEST_DIR"));
+
         println!("cargo:rustc-link-lib=c++");
     }
 
