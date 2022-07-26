e8d14544284af9e945c22a00bb2b2eac10855c54
Chris <chris@tempus-ex.com>
2022-07-26 14:55:47 -0400

    add rsrc_init api (#104)
    * add rsrc_init api
    
    * fix arg types
    
    * add message format
    
    * compilation fixes

---
+++ xcoder/xcoder-quadra/src/common.rs
@@ -0,0 +1,17 @@
+use snafu::Snafu;
+use xcoder_quadra_sys as sys;
+
+#[derive(Debug, Snafu)]
+pub enum XcoderInitError {
+    #[snafu(display("error (code = {code})"))]
+    Unknown { code: sys::ni_retcode_t },
+}
+
+#[cfg(target_os = "linux")]
+pub fn init(should_match_rev: bool, timeout_seconds: u32) -> Result<(), XcoderInitError> {
+    let code = unsafe { sys::ni_rsrc_init(if should_match_rev { 1 } else { 0 }, timeout_seconds as _) };
+    if code != sys::ni_retcode_t_NI_RETCODE_SUCCESS {
+        return Err(XcoderInitError::Unknown { code });
+    }
+    Ok(())
+}
--- xcoder/xcoder-quadra/src/lib.rs
+++ xcoder/xcoder-quadra/src/lib.rs
@@ -1,5 +1,11 @@
 #[cfg(target_os = "linux")]
+mod common;
+
+#[cfg(target_os = "linux")]
 mod encoder;
+
+#[cfg(target_os = "linux")]
+pub use common::*;
 
 #[cfg(target_os = "linux")]
 pub use encoder::*;
