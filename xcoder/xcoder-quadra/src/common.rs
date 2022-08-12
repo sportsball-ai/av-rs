cfe2c562eafd456310c7b80ed2b5a96b2be9e39a
ccbrown <chris@tempus-ex.com>
2022-08-12 06:48:09 +0000

    add decoding to quadra-decoder crate

--- xcoder/xcoder-quadra/src/common.rs
+++ xcoder/xcoder-quadra/src/common.rs
@@ -7,6 +7,12 @@ pub enum XcoderInitError {
     Unknown { code: sys::ni_retcode_t },
 }
 
+#[derive(Debug, Clone)]
+pub struct XcoderHardware {
+    pub id: i32,
+    pub device_handle: i32,
+}
+
 #[cfg(target_os = "linux")]
 pub fn init(should_match_rev: bool, timeout_seconds: u32) -> Result<(), XcoderInitError> {
     let code = unsafe { sys::ni_rsrc_init(if should_match_rev { 1 } else { 0 }, timeout_seconds as _) };
@@ -14,4 +20,16 @@ pub fn init(should_match_rev: bool, timeout_seconds: u32) -> Result<(), XcoderIn
         return Err(XcoderInitError::Unknown { code });
     }
     Ok(())
+}
+
+pub(crate) fn fps_to_rational(fps: f64) -> (i32, i32) {
+    let den = if fps.fract() == 0.0 {
+        1000
+    } else {
+        // a denominator of 1001 for 29.97 or 59.94 is more
+        // conventional
+        1001
+    };
+    let num = (fps * den as f64).round() as _;
+    (den, num)
 }
