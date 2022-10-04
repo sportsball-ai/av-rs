0d02b9ee7f669a32521344306d5cab9c3267bd6c
Chris <chris@tempus-ex.com>
2022-10-04 12:52:48 -0400

    xilinx large packet segfault protection (#134)
    * xilinx segfault protection
    
    * use ErrorInvalid for xilinx error type
    
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-92-96.ec2.internal>

--- xilinx/src/xlnx_error.rs
+++ xilinx/src/xlnx_error.rs
@@ -3,14 +3,16 @@ use std::fmt;
 
 use crate::XMA_TRY_AGAIN;
 
-#[derive(Debug, PartialEq)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub enum XlnxErrorType {
-    XlnxTryAgain,
-    XlnxEOS,
-    XlnxErr,
-    XlnxSendMoreData,
-    XlnxFlushAgain,
+    TryAgain,
+    EOS,
+    SendMoreData,
+    FlushAgain,
+    ErrorInvalid,
+    Other,
 }
+
 pub struct XlnxError {
     pub err: XlnxErrorType,
     pub message: String,
@@ -22,13 +24,15 @@ impl XlnxError {
         const EOS: i32 = XMA_EOS as i32;
         const SEND_MORE_DATA: i32 = XMA_SEND_MORE_DATA as i32;
         const FLUSH_AGAIN: i32 = XMA_FLUSH_AGAIN as i32;
+        const ERROR_INVALID: i32 = XMA_ERROR_INVALID as i32;
 
         let err = match xma_error {
-            TRY_AGAIN => XlnxErrorType::XlnxTryAgain,
-            EOS => XlnxErrorType::XlnxEOS,
-            SEND_MORE_DATA => XlnxErrorType::XlnxSendMoreData,
-            FLUSH_AGAIN => XlnxErrorType::XlnxFlushAgain,
-            _ => XlnxErrorType::XlnxErr,
+            TRY_AGAIN => XlnxErrorType::TryAgain,
+            EOS => XlnxErrorType::EOS,
+            SEND_MORE_DATA => XlnxErrorType::SendMoreData,
+            FLUSH_AGAIN => XlnxErrorType::FlushAgain,
+            ERROR_INVALID => XlnxErrorType::ErrorInvalid,
+            _ => XlnxErrorType::Other,
         };
 
         let message = match message {
