e025921efc6d0fca111f9169d03ec539ea34df1e
Scott Lamb <scott.lamb@tempus-ex.com>
2024-11-27 08:40:20 -0800

    release object more explicitly (#211)
    ...to avoid build failure due to the following lint error, I
    assume since some compiler upgrade.
    
    ```
    warning: foundation@0.1.0: src/lib.mm:9:12: warning: variable 'm' set but not used [-Wunused-but-set-variable]
    warning: foundation@0.1.0:         id m = (__bridge_transfer id)obj;
    warning: foundation@0.1.0:            ^
    warning: foundation@0.1.0: 1 warning generated.
    ```
    
    My Objective-C is quite rusty (pardon the pun), but my understanding
    is...
    
    * `__bridge_transfer` converts a `CFObject` to a `NSObject`.
    * `@autoreleasepool` releases the object on leaving the block.
    
    Let's just call `CFRelease` instead.

--- macos/foundation/build.rs
+++ macos/foundation/build.rs
@@ -4,7 +4,7 @@ fn main() {
         println!("cargo:rustc-link-lib=framework=Foundation");
 
         cc::Build::new()
-            .files(["src/lib.mm", "src/nserror.mm", "src/nsstring.mm"].iter())
+            .files(["src/nserror.mm", "src/nsstring.mm"].iter())
             .flag("-fobjc-arc")
             .compile("foundation");
     }
