afbdaa8d183fe0b5c3ed1cf12d9aa29c4dd53c3f
Jacob Kiesel <jacob.kiesel@infiniteathlete.ai>
2025-02-13 23:34:33 +0000

    Go up another level when getting cuda path

--- nvidia-video-sdk/build.rs
+++ nvidia-video-sdk/build.rs
@@ -3,12 +3,12 @@ use std::{env, path::PathBuf};
 use which::which;
 
 fn main() {
-    let cuda_folder = which("nvcc").unwrap().parent().unwrap().to_path_buf();
+    let cuda_folder = which("nvcc").unwrap().parent().unwrap().parent().unwrap().to_path_buf();
     let bindings = bindgen::builder()
+        .clang_arg(format!("-I{}", cuda_folder.join("include").display()))
         .header("src/vendored-headers/nvcuvid.h")
         .header("src/vendored-headers/cuviddec.h")
         .header("src/vendored-headers/nvEncodeAPI.h")
-        .clang_arg(format!("-I{}", cuda_folder.join("include").display()))
         .allowlist_item("nv*")
         .allowlist_item("cuda*")
         .allowlist_item("CU*")
