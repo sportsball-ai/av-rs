d98b9a11a647ba4f3076e97723ef6bfdfd939ebf
Jacob Kiesel <jacob.kiesel@infiniteathlete.ai>
2025-02-13 16:14:44 -0700

    find and use cuda folder

--- nvidia-video-sdk/build.rs
+++ nvidia-video-sdk/build.rs
@@ -1,10 +1,14 @@
 use std::{env, path::PathBuf};
 
+use which::which;
+
 fn main() {
+    let cuda_folder = which("nvcc").unwrap().parent().unwrap().to_path_buf();
     let bindings = bindgen::builder()
         .header("src/vendored-headers/nvcuvid.h")
         .header("src/vendored-headers/cuviddec.h")
         .header("src/vendored-headers/nvEncodeAPI.h")
+        .clang_arg(format!("-I{}", cuda_folder.join("include").display()))
         .allowlist_item("nv*")
         .allowlist_item("cuda*")
         .allowlist_item("CU*")
